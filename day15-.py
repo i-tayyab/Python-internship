a3fc5c9d325a1a29bcdd567a7e8841744b83d07a
i-tayyab <tayyabbashir757@gmail.com>
2025-07-21 14:36:48 +0500

    Create day15-.py
    ...

---
+++ day15-.py
@@ -0,0 +1,22 @@
+## Project Title:
+Smart Expense Tracker
+
+## Objective:
+To help individuals monitor, categorize, and analyze their expenses effectively using a user-friendly Python application.
+
+## Features:
+- Add/Delete/View expenses
+- Auto-categorization using keywords
+- Monthly summary reports
+- Export to Excel/CSV
+
+## Tools/Libraries:
+- Python, Pandas, Tkinter (for GUI), Matplotlib
+
+## Timeline:
+Week 1: UI & Core Functions  
+Week 2: Data handling & Reporting  
+Week 3: Testing & GitHub Push
+
+## Team Members & Roles:
+Tayab – Solo Project
